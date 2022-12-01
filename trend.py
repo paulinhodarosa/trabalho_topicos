dde4314580361a61ff9a5038c37989dddc70f81a
Paulo Henrique da Rosa <101262901+paulinhodarosa@users.noreply.github.com>
2022-12-01 09:03:07 -0300

    Update trend.py

--- trend.py
+++ trend.py
@@ -6,13 +6,30 @@ from sklearn.linear_model import LinearRegression #Regressão linear
 from sklearn import metrics #Cálculo do erro
 import matplotlib_inline
 import matplotlib
+from sklearn import tree
 
 # carrega a base de dados
 df = pd.read_csv('drugs/drugs_and_riskfactors.csv')
 
+# df = {
+#     "age": [1],
+#     "education": [2],
+#     "father": [4],
+#     "mother": [8],
+#     "income": [16],
+#     "race": [32],
+#     "sex": [64],
+#     "employment": [128],
+#     "alcohol": [256],
+#     "cigarettes": [512], 
+#     "cocaine": [1024],
+#     "crack": [2048], 
+#     "heroin": [4096],
+#     "marijuana": [8192], 
+#     "meth": [16384]
+# }
 
-
-quant = ["age", "education",
+quali = ["age", "education",
         "father", "mother", 
         "income", "race",
         "sex", "employment", 
@@ -20,9 +37,19 @@ quant = ["age", "education",
         "cocaine", "crack", 
         "heroin", "marijuana", 
         "meth"
-        ]
+] #variaveis quantitativas
 
-dfQuant = df[quant]
+
+dfQuali = df[quali] #Dataframe com quantitativas
+alvo = 'crack' #variável alvo
+target = df[alvo] #Separa a variável alvo
+
+pd.get_dummies(df['father'])
+pd.get_dummies(df['mother'])
+pd.get_dummies(df['income'])
+pd.get_dummies(df['sex'])
+pd.get_dummies(df['age'])
+
 
 usuario = ['Sim', 'Não']
 
@@ -63,9 +90,82 @@ age_count = df['age'].unique()
 age_min = int(df['age'].min())
 age_max = int(df['age'].max())
 
-age = st.slider('Selecione as faixas etarias [1-6] ', age_min, age_max, value=age_max)
-escolaridade = st.slider('Informe o índice de escolaridade: ', ed_min, ed_max, value=ed_min)
+income_count = df['income'].unique()
+income_min = int(df['income'].min())
+income_max = int(df['income'].max())
+
+gender = st.slider('Selecione as faixas etarias [1-6] ', sex_min, sex_max, value=sex_min)
+age = st.slider('Selecione as faixas etarias [1-6] ', age_min, age_max, value=age_min)
+escolaridade = st.slider('Registre o índice de escolaridade da pessoa: ', ed_min, ed_max, value=ed_min)
+renda_registro = st.slider('Registre a renda da pessoa: ', income_min, income_max, value=income_min)
 ausencia_pai = st.radio('A pessoa tem o Pai Ausente? ', options=pai)
 ausencia_mae = st.radio('A pessoa tem a Mae Ausente? ', options=mae)
+
+pd.get_dummies(df[quali])
+
+# condicaoidade = df['age']=='1'
+# UsaSim, UsaNao =  df[condicaoidade] , df[~condicaoidade]
+
+# df = UsaNao
+# condicao = df['father']='1', '2'
+# condicao2 = df['mother']='1', '2'
+# condicao3 = df['education']='2', '4'
+# UsaNao_paipresente = df[condicao]
+# UsaNao_paiausente = df[~condicao]
+# UsaNao_maepresente = df[condicao2]
+# UsaNao_maeausente = df[~condicao2]
+# UsaNao_edalta = df[condicao3]
+# UsaNao_edbaixa= df[~condicao3]
+
+vals = [0,  #pai_presente
+        0,  #pai_ausente
+        0,  #mae_presente
+        0,  #mae_ausente
+        0,  #renda alta
+        0,  #renda baixa
+        0,  #jovem
+        0, #adulto
+        0, #masculino
+        0 #feminino
+       ]
+
+arv = tree.DecisionTreeRegressor() #árvore de regressão
+arv.fit(dfQuali, target) #cria a árvore
+res = arv.predict([vals])   
+
+#pai presente
+if ausencia_pai in ['2', '1']:
+    vals[0] = 1
+elif ausencia_pai in ['3', '4']:
+    vals[1] = 1
+#mae presente
+if ausencia_mae in ['2', '1']:
+    vals[2] = 1
+elif ausencia_mae in ['3', '4']:
+    vals[3] = 1
+
+#renda 
+if renda_registro in ['2', '1']: #menos de 49 mil
+    vals[4] = 1
+elif renda_registro in ['3', '4']: #mais de 50 mil
+    vals[5] = 1
+
+# idade
+if renda_registro in ['2', '1']: #JOVEM
+    vals[6] = 1
+elif renda_registro in ['3', '4', '5', '6']: #ADULTO
+    vals[7] = 1
+
+if renda_registro == '1': #masculino
+    vals[8] = 1
+elif renda_registro == 2: #ADULTO
+    vals[9] = 1
+
+
+
+if res[0] == '1':
+    st.write('A pessoa tem a tendencia a não usar crack')
+else:
+    st.write("A pessoa tem a tendencia a usar crack")
+
 # vento_dia = st.selectbox('Está ventando?: ', vento)
- 
